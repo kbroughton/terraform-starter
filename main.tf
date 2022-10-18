62c26383fde84cee9add8f8416e58105e22d1ac4
kbroughton <kesten.broughton@gmail.com>
2022-10-18 12:26:00 -0500

    aws

--- main.tf
+++ main.tf
@@ -3,5 +3,28 @@ terraform {
     spacelift = {
       source = "spacelift-io/spacelift"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
 }
