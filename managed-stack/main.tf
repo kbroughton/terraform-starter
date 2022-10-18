35b27a6cc3971b274f6b68d516bebe51d5ef75ef
kbroughton <kesten.broughton@gmail.com>
2022-10-18 13:17:25 -0500

    managed

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -4,3 +4,18 @@ resource "random_password" "secret" {
   length  = 8
   special = true
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
