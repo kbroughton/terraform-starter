2d65febdea885cad9caf7eb844b3521f5f8befad
kbroughton <kesten.broughton@gmail.com>
2022-10-18 12:05:07 -0500

    policies

--- policies.tf
+++ policies.tf
@@ -8,8 +8,4 @@
 # https://docs.spacelift.io/concepts/policy/stack-access-policy
 
 # Access policies only take effect when attached to the stack.
-resource "spacelift_policy_attachment" "access" {
-  policy_id = spacelift_policy.access.id
-  stack_id  = spacelift_stack.managed.id
-}
 
