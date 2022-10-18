65909ce927147f3ea9f114377c80d29d15228c3e
kbroughton <kesten.broughton@gmail.com>
2022-10-18 13:18:51 -0500

    managed

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -11,6 +11,19 @@ provider "aws" {
 
 resource "aws_iam_role" "test_role" {
   name = "test_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
 data "aws_caller_identity" "current" {}
